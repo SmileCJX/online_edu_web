df4d4e9292e437f9bb60daa4dc0c4f924dd414fa
SmileCJX <1092925421@qq.com>
2020-07-08 21:25:05 +0800

    first commit

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+export default {
+    // 1 讲师列表 （条件查询分页）
+    getTeacherListPage(current, limit, teacherQuery) {
+        return request({
+            // url: '/table/list' + current + "/" + limit
+            url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`
+            ,method: 'post'
+            // teacherQuery条件对象，后端使用RequestBody获取数据
+            // data表示把对象转换json进行传递到接口里面
+            ,data: teacherQuery
+        })
+    }
+    // 删除讲师
+    ,deleteTeacherId(id) {
+        return request({
+            url: `/eduservice/teacher/${id}`
+            ,method: 'delete'
+        })
+    }
+    ,addTeacher(teacher) {
+        return request({
+            url: `/eduservice/teacher/addTeacher`
+            ,method: 'post'
+            ,data: teacher
+        })
+    }
+    // 根据id查询讲师
+    ,getTeacherInfo(id) {
+        return request({
+            url: `/eduservice/teacher/getTeacher/${id}`
+            ,method: 'get'
+        })
+    }
+    ,updateTeacherInfo(teacher) {
+        return request({
+            url: `/eduservice/teacher/updateTeacher`
+            ,method: 'post'
+            ,data: teacher
+        })
+    }
+}
