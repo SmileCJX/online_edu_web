3e23555fc06e3ac5bfe81ae5ce660109ad2f2fcf
SmileCJX <1092925421@qq.com>
2020-07-22 00:15:09 +0800

    添加课程信息

--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -12,17 +12,51 @@
 
     <el-form label-width="120px">
 
-      <el-form-item>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next">保存并下一步</el-button>
-      </el-form-item>
+        <el-form-item label="课程标题">
+            <el-input v-model="courseInfo.title" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
+        </el-form-item>
+
+        <!-- 所属分类 TODO -->
+
+        <!-- 课程讲师 TODO -->
+
+        <el-form-item label="总课时">
+            <el-input-number :min="0" v-model="courseInfo.lessonNum" controls-position="right" placeholder="请填写课程的总课时数"/>
+        </el-form-item>
+
+        <!-- 课程简介 TODO -->
+        <el-form-item label="课程简介">
+            <el-input v-model="courseInfo.description" placeholder=""/>
+        </el-form-item>
+
+
+        <!-- 课程封面 TODO -->
+
+        <el-form-item label="课程价格">
+            <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元"/> 元
+        </el-form-item>
+
+        <el-form-item>
+            <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存并下一步</el-button>
+        </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
+import course from '@/api/edu/course'
 export default {
   data() {
     return {
       saveBtnDisabled: false // 保存按钮是否禁用
+      ,courseInfo: {
+            title: '',
+            subjectId: '',
+            teacherId: '',
+            lessonNum: 0,
+            description: '',
+            cover: '',
+            price: 0
+      }
     }
   },
 
@@ -32,9 +66,17 @@ export default {
 
   methods: {
 
-    next() {
-      console.log('next')
-      this.$router.push({ path: '/edu/course/chapter/1' })
+    saveOrUpdate() {
+      course.addCourseInfo(this.courseInfo)
+        .then(response => {
+            // 提示
+            this.$message({
+                type: 'success',
+                message: '添加课程信息成功'
+            })
+            this.$router.push({ path: '/course/chapter/' + response.data.courseId })
+        })
+      
     }
   }
 }
