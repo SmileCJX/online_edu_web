00fcd1edc35315efc86864ee9a47acbd45c7de84
SmileCJX <1092925421@qq.com>
2020-07-21 23:04:00 +0800

    课程路径

--- src/router/index.js
+++ src/router/index.js
@@ -89,6 +89,28 @@ export const constantRouterMap = [
   },
 
   {
+    path: '/course',
+    component: Layout,
+    redirect: '/course/list',
+    name: '课程管理',
+    meta: { title: '课程管理', icon: 'example' },
+    children: [
+      {
+        path: 'list',
+        name: '课程列表',
+        component: () => import('@/views/edu/subject/list'),
+        meta: { title: '课程分类列表', icon: 'table' }
+      },
+      {
+        path: 'save',
+        name: '添加课程',
+        component: () => import('@/views/edu/subject/save'),
+        meta: { title: '添加课程', icon: 'tree' }
+      }
+    ]
+  },
+
+  {
     path: '/example',
     component: Layout,
     redirect: '/example/table',
